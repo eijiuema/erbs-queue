ae19b00b0c2acb76d85071765a04020afea0d66e
Eiji Uema <eijiuema@gmail.com>
2021-10-05 13:46:19 -0300

    .

--- src/Queue.svelte
+++ src/Queue.svelte
@@ -26,20 +26,18 @@
   };
 </script>
 
-<div class="container">
+<div class="container" class:active={joined}>
   <div class="card" style="background-image: url({img})" on:click={handleClick}>
-    <div class="count">
+    <div class="box">
       {sum(ranks.flat())}
       {sum(ranks.flat()) == 1 ? "jogador" : "jogadores"}
     </div>
     {#if joined}
-      <div class="joined">
-        <div class="check">Pareando...</div>
-      </div>
+      <div class="box center">Sair da fila</div>
     {:else}
-      <div />
+      <div class="box center">Entrar na fila</div>
     {/if}
-    <div class="name">{name}</div>
+    <div class="box">{name}</div>
   </div>
 
   <div class="chart">
@@ -49,32 +47,37 @@
 
 <style>
   .container {
+    box-sizing: border-box;
     display: flex;
     overflow-x: auto;
     height: 355px;
+    border: 5px solid transparent;
+  }
+
+  .container.active {
+    border-color: green;
   }
 
   .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     user-select: none;
     flex-shrink: 0;
     width: 229px;
     height: 100%;
     background-repeat: no-repeat;
     background-size: cover;
-    position: relative;
   }
 
   .card:hover {
     cursor: pointer;
   }
 
-  .name {
-    display: inline-block;
+  .box {
+    width: 100%;
     box-sizing: border-box;
     padding: 8px;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
     color: white;
     font-style: italic;
     text-align: right;
@@ -86,40 +89,7 @@
     font-size: 2em;
   }
 
-  .count {
-    display: inline-block;
-    box-sizing: border-box;
-    padding: 8px;
-    position: absolute;
-    top: 0;
-    width: 100%;
-    color: white;
-    font-style: italic;
-    text-align: right;
-    background-color: rgba(0, 0, 0, 0.6);
-    background-size: 50% 50%;
-    background-position: 50% 50%;
-    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
-      1px 1px 0 #000;
-    font-size: 1.5em;
-  }
-
-  .joined {
-    width: 100%;
-    height: 50%;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    background: rgba(0, 0, 0, 0.6);
-  }
-
-  .check {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 2em;
-    color: white;
+  .center {
     text-align: center;
   }
 
